b7b2cd4c254ddbaf41010a8544b433b823f77d11
CseEngineerAsif <asifcse19@gmail.com>
2026-03-01 21:58:10 +0600

    Add problem statement for distinct numbers task

--- 1_DistinctNumbers.cpp
+++ 1_DistinctNumbers.cpp
@@ -1,3 +1,10 @@
+/* problem:You are given a list of n integers, and your task is to calculate the number of distinct values in the list.
+Input
+The first input line has an integer n: the number of values.
+The second line has n integers x1,x2,....,xn.
+Output
+Print one integers: the number of distinct values. */
+
 #include <bits/stdc++.h>
 using namespace std;
 
