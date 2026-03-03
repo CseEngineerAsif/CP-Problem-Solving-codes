7f955c975744d2795662aee3ed9b4eaa228ebc31
CseEngineerAsif <asifcse19@gmail.com>
2026-03-03 22:19:15 +0600

    Add files via upload

--- 1_DistinctNumbers.cpp
+++ 1_DistinctNumbers.cpp
@@ -1,26 +1,18 @@
-/* problem:You are given a list of n integers, and your task is to calculate the number of distinct values in the list.
-Input
-The first input line has an integer n: the number of values.
-The second line has n integers x1,x2,....,xn.
-Output
-Print one integers: the number of distinct values. */
-
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
-    
     set<int> s;
-    
+
     for(int i = 0; i < n; i++) {
         int x;
         cin >> x;
         s.insert(x);
     }
-    
+
     cout << s.size() << endl;
-    
+
     return 0;
 }
