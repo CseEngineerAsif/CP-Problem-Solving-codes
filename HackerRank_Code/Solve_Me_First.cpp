9b6f9a6097192eb9d624052a8c3c18040cec1a2f
Md. Asif Hasan <asifcse19@gmail.com>
2026-06-05 20:13:35 +0600

    Add Solve Me First problem solution

---
+++ HackerRank_Code/Solve_Me_First.cpp
@@ -0,0 +1,16 @@
+//https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+#include <iostream>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << solveMeFirst(a, b);
+
+    return 0;
+}
