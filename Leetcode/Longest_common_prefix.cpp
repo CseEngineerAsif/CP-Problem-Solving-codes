448a685e17b400dbb9e0bc30796c543ebf2312d3
Md. Asif Hasan <asifcse19@gmail.com>
2026-09-19 21:51:55 +0600

    Add longestCommonPrefix function in Longest_common_prefix.cpp
    Implement function to find the longest common prefix among an array of strings.

---
+++ Leetcode/Longest_common_prefix.cpp
@@ -0,0 +1,40 @@
+//https://leetcode.com/problems/longest-common-prefix/description/?envType=problem-list-v2&envId=array
+#include <bits/stdc++.h>
+using namespace std;
+
+string longestCommonPrefix(vector<string> strs)
+{
+    string ans = strs[0];
+
+    for (int i = 1; i < strs.size(); i++)
+    {
+        int j = 0;
+
+        while (j < ans.length() &&
+               j < strs[i].length() &&
+               ans[j] == strs[i][j])
+        {
+            j++;
+        }
+
+        ans = ans.substr(0, j);
+
+        if (ans == "")
+        {
+            return "";
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    vector<string> strs = {"flower", "flow", "flight"};
+
+    string result = longestCommonPrefix(strs);
+
+    cout << "Longest Common Prefix: " << result << endl;
+
+    return 0;
+}
